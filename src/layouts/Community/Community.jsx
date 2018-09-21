69bd0f06bc2d96823e6a6e23d1f4d0eddce870fd
James Sayer <jamesdsayer3@gmail.com>
2018-09-20 21:26:09 -0500

    create Community layout

---
+++ src/layouts/Community/Community.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Article from "../../components/Article/Article";
+import { community } from "../../../data/SiteConfig";
+import communityHeaderIcon from "../../../static/assets/community/community_header_icon.svg"
+import './Community.css';
+
+class Community extends React.Component {
+  render() {
+    return (
+      <div className="community">
+        <div className="community-header">
+          <div className="community-header-title">
+            <img src={communityHeaderIcon} alt="header icon" />
+            <h3>Ask the community</h3>
+          </div>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing<br />
+            elit quisque congue massa quis nisi vehicula
+          </span>
+        </div>
+        <div className="article-content">
+          {Object.values(community).map(item => (
+            <Article
+              imgUrl={item}
+              title="Question"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Community;
